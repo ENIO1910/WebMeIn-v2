ec2c8000981d6e98a05afeb523a9bd1788d4e98a
ENIO1910 <patrykstaniewski1910@gmail.com>
2023-01-06 16:36:34 +0100

    Dodanie listy lekcji Dodawanie lekcji Dodawanie plików do lekcji

---
+++ database/migrations/2023_01_05_121435_create_lessons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('number',100);
+            $table->string('name',500);
+            $table->string('description',1500);
+            $table->string('file_path', 300);
+            $table->unsignedBigInteger('course_id')->nullable();
+            $table->timestamps();
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
